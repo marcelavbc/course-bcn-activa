cccb066e55e74a737cc8856102a9ee999206b22b
marcela <marcela.vilasboas@gmail.com>
2021-02-23 18:46:50 +0100

    done

---
+++ modal-transitions/src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <button class="button" @click="showDialog">Show Modal</button>
+  <transition name="fade" appear>
+    <div class="modal-overlay" v-if="dialogIsVisible" @click="hideDialog"></div>
+  </transition>
+  <the-modal @close="hideDialog" :open="dialogIsVisible">
+    <button class="button" @click="hideDialog">Close it!</button>
+  </the-modal>
+</template>
+
+<script>
+import TheModal from "./components/Modal";
+export default {
+  name: "App",
+  components: {
+    TheModal,
+  },
+  data() {
+    return {
+      dialogIsVisible: false,
+    };
+  },
+  methods: {
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
+#app {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+.button {
+  background-color: rgb(106, 90, 205);
+  border: none;
+  outline: none;
+  color: white;
+  padding: 15px 25px;
+  font-size: 18px;
+  font-weight: 700;
+  border-radius: 8px;
+  box-shadow: 3px 3px rgba(0, 0, 0, 0.4);
+  transition: 0.4s ease-out;
+}
+
+.button:hover {
+  box-shadow: 6px 6px rgba(0, 0, 0, 0.6);
+}
+
+.modal-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 98;
+  background-color: rgba(106, 90, 205, 1);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1.5s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ modal-transitions/src/components/Modal.vue
@@ -0,0 +1,59 @@
+<template>
+  <transition name="slide">
+    <div class="modal" v-if="open">
+      <h1>Lorem Ipsum</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        provident explicabo accusamus laudantium voluptatum nobis sed nesciunt
+        neque possimus molestiae?
+      </p>
+      <button class="button" @click="$emit('close')">Close Modal</button>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: ["open"],
+  emits: ["close"],
+};
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+  width: 100%;
+  max-width: 400px;
+  background-color: #fff;
+  border-radius: 16px;
+  padding: 25px;
+}
+
+h1 {
+  color: #222;
+  font-size: 32px;
+  font-weight: 900;
+  margin-bottom: 15px;
+}
+
+p {
+  color: #666;
+  font-size: 18px;
+  font-weight: 400;
+  margin-bottom: 15px;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 1s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(-50%) translateX(100vw);
+}
+</style>
